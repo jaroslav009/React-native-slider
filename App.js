b0453b849e4561800246370aa1e57842d89a680c
jaroslav009 <jaroslav.iliuk@gmail.com>
2019-06-24 22:34:14 +0300

    work

--- App.js
+++ App.js
@@ -42,7 +42,7 @@ const AppNavigator = createStackNavigator({
     QuizShowProfile: {screen : QuizShowProfile}
   },
   {
-    initialRouteName: 'Profile',
+    initialRouteName: 'WrapSlider',
     headerMode: 'none'
   }
 );
